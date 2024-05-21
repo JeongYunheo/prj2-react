32352a9bd713df8357d451963981f1fcce9a8c7c
jeongY <zoe1126@naver.com>
2024-05-21 14:39:00 +0900

    게시물 목록 테이블의 행 클릭 시 게시물 보기로 이동

--- src/page/BoardList.jsx
+++ src/page/BoardList.jsx
@@ -3,9 +3,11 @@ import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get("/api/board/list").then((res) => setBoardList(res.data));
@@ -25,7 +27,12 @@ export function BoardList() {
           </Thead>
           <Tbody>
             {boardList.map((board) => (
-              <Tr key={board.id}>
+              <Tr
+                onClick={() => navigate(`/board/${board.id}`)}
+                key={board.id}
+                cursor={"pointer"}
+                _hover={{ bgColor: "gray.200" }}
+              >
                 <Td>{board.id}</Td>
                 <Td>{board.title}</Td>
                 <Td>{board.writer}</Td>
