3d9b50e59a0bb6eaf28b8783506b4ef87029e095
jeongY <zoe1126@naver.com>
2024-05-21 11:17:28 +0900

    /, /write 경로와 Home, BoardWriter 컴포넌트 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./page/Home.jsx";
+import { BoardWriter } from "./page/BoardWriter.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [{ path: "write", element: <BoardWriter /> }],
+  },
+]);
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  );
 }
 
 export default App;
---
+++ src/page/BoardWriter.jsx
@@ -0,0 +1,6 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+export function BoardWriter() {
+  return <Box>글 작성 화면</Box>;
+}
