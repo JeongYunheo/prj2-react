f05051ac2d35db96faee9ed544d79b0a7e74e0fe
jeongY <zoe1126@naver.com>
2024-05-22 16:24:00 +0900

    회원 정보 삭제

--- src/page/member/MemberView.jsx
+++ src/page/member/MemberView.jsx
@@ -12,12 +12,16 @@ import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 
 export function MemberView() {
-  const { id } = useParams();
   const [member, setMember] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { id } = useParams();
+
   const toast = useToast();
   const navigate = useNavigate();
 
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get(`/api/member/${id}`)
       .then((res) => setMember(res.data))
@@ -33,9 +37,33 @@ export function MemberView() {
       });
   }, []);
 
+  function handleClickRemove() {
+    axios
+      .delete(`/api/member/${id}`)
+      .then(() => {
+        toast({
+          status: "success",
+          description: "회원 탈퇴되었습니다",
+          position: "top",
+        });
+        navigate("/");
+      })
+      .catch(() => {
+        toast({
+          status: "warning",
+          description: "회원 탈퇴 중 문제가 발생하였습니다",
+          position: "top",
+        });
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
   if (member == null) {
     return <Spinner />;
   }
+
   return (
     <Box>
       <Box> 회원 정보</Box>
@@ -61,7 +89,13 @@ export function MemberView() {
       </Box>
       <Box>
         <Button colorScheme={"green"}>수정</Button>
-        <Button colorScheme={"red"}>삭제</Button>
+        <Button
+          isLoading={isLoading}
+          colorScheme={"red"}
+          onClick={handleClickRemove}
+        >
+          탈퇴
+        </Button>
       </Box>
     </Box>
   );
