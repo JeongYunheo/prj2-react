1a9dad8bd5492d6d2589aead3aa44e4f4a327aa7
jeongY <zoe1126@naver.com>
2024-05-22 11:27:46 +0900

    이메일, 별명 중복 확인 되어야 가입 버튼 활성화

--- src/page/member/MemberSignup.jsx
+++ src/page/member/MemberSignup.jsx
@@ -18,7 +18,10 @@ export function MemberSignup() {
   const [password, setPassword] = useState("");
   const [passwordCheck, setPasswordCheck] = useState("");
   const [nickName, setNickName] = useState("");
+  const [isCheckedEmail, setIsCheckedEmail] = useState(false);
+  const [isCheckedNickName, setIsCheckedNickName] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+
   const toast = useToast();
   const navigate = useNavigate();
 
@@ -70,6 +73,7 @@ export function MemberSignup() {
             description: "사용할 수 있는 이메일입니다",
             position: "top",
           });
+          setIsCheckedEmail(true);
         }
       })
       .finally();
@@ -93,6 +97,7 @@ export function MemberSignup() {
             description: "사용할 수 있는 별명입니다",
             position: "top",
           });
+          setIsCheckedNickName(true);
         }
       })
       .finally();
@@ -116,6 +121,13 @@ export function MemberSignup() {
     isDisabled = true;
   }
 
+  if (!isCheckedEmail) {
+    isDisabled = true;
+  }
+  if (!isCheckedNickName) {
+    isDisabled = true;
+  }
+
   return (
     <Box>
       <Box>회원가입</Box>
@@ -124,7 +136,12 @@ export function MemberSignup() {
           <FormControl>
             <FormLabel>이메일</FormLabel>
             <InputGroup>
-              <Input onChange={(e) => setEmail(e.target.value)} />
+              <Input
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                  setIsCheckedEmail(false);
+                }}
+              />
               <InputRightElement w={"75px"} mr={1}>
                 <Button onClick={handleCheckEmail} size={"sm"}>
                   중복확인
@@ -152,7 +169,12 @@ export function MemberSignup() {
           <FormControl>
             <FormLabel>닉네임</FormLabel>
             <InputGroup>
-              <Input onChange={(e) => setNickName(e.target.value)} />
+              <Input
+                onChange={(e) => {
+                  setNickName(e.target.value);
+                  setIsCheckedNickName(false);
+                }}
+              />
               <InputRightElement w={"75px"} mr={1}>
                 <Button size={"sm"} onClick={handleClickNickName}>
                   중복확인
