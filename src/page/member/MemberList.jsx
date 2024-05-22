26a582700dda8ddc50e03606bebfbc480f7b0ebf
jeongY <zoe1126@naver.com>
2024-05-22 15:01:00 +0900

    회원 정보 화면 연습

--- src/page/member/MemberList.jsx
+++ src/page/member/MemberList.jsx
@@ -10,9 +10,13 @@ import {
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarCheck } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 export function MemberList() {
   const [memberList, setMemberList] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get("/api/member/list").then((res) => setMemberList(res.data));
@@ -21,6 +25,7 @@ export function MemberList() {
   if (memberList.length === 0) {
     return <Spinner />;
   }
+
   return (
     <Box>
       <Box>회원목록</Box>
@@ -31,12 +36,18 @@ export function MemberList() {
               <Th>#</Th>
               <Th>이메일</Th>
               <Th>별명</Th>
-              <Th>가입일시</Th>
+              <Th>
+                <FontAwesomeIcon icon={faCalendarCheck} />
+              </Th>
             </Tr>
           </Thead>
           <Tbody>
             {memberList.map((member) => (
-              <Tr key={member.id}>
+              <Tr
+                key={member.id}
+                _hover={{ bgColor: "gray.200" }}
+                onClick={() => navigate(`/member/${member.id}`)}
+              >
                 <Td>{member.id}</Td>
                 <Td>{member.email}</Td>
                 <Td>{member.nickName}</Td>
---
+++ src/page/member/MemberView.jsx
@@ -0,0 +1,30 @@
+import { Box, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
+export function MemberView() {
+  const { id } = useParams();
+  const [member, setMember] = useState(null);
+
+  return (
+    <Box>
+      <Box>{member.id}번 회원 정보</Box>
+      <Box>
+        <Box>
+          <FormControl>
+            <FormLabel>이메일</FormLabel>
+            <Input value={member.email} readOnly />
+          </FormControl>
+          <FormControl>
+            <FormLabel>닉네임</FormLabel>
+            <Input value={member.nickName} readOnly />
+          </FormControl>
+          <FormControl>
+            <FormLabel>가입일자</FormLabel>
+            <Input value={member.inserted} readOnly />
+          </FormControl>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
