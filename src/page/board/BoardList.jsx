61ee6d3866b4d9d6ec7bcacc0f14671164177851
jeongY <zoe1126@naver.com>
2024-05-27 12:18:14 +0900

    이전, 처음, 다음, 맨끝 버튼 만들기

--- src/page/board/BoardList.jsx
+++ src/page/board/BoardList.jsx
@@ -54,6 +54,16 @@ export function BoardList() {
         </Table>
       </Box>
       <Box>
+        {pageInfo.prevPageNumber && (
+          <>
+            <Button onClick={() => navigate(`/?page=1`)}>처음</Button>
+            <Button
+              onClick={() => navigate(`/?page=${pageInfo.prevPageNumber}`)}
+            >
+              이전
+            </Button>
+          </>
+        )}
         {pageNumbers.map((pageNumber) => (
           <Button
             onClick={() => navigate(`/?page=${pageNumber}`)}
@@ -65,6 +75,20 @@ export function BoardList() {
             {pageNumber}
           </Button>
         ))}
+        {pageInfo.nextPageNumber && (
+          <>
+            <Button
+              onClick={() => navigate(`/?page=${pageInfo.nextPageNumber}`)}
+            >
+              다음
+            </Button>
+            <Button
+              onClick={() => navigate(`/?page=${pageInfo.lastPageNumber}`)}
+            >
+              맨끝
+            </Button>
+          </>
+        )}
       </Box>
     </Box>
   );
