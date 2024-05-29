2c9417c518dca4cd8875f3f44f53e168e614988e
jeongY <zoe1126@naver.com>
2024-05-29 15:06:42 +0900

    좋아요 하트 출력 / 좋아요 {like, count} 객체로 상태 관리

--- src/page/board/BoardView.jsx
+++ src/page/board/BoardView.jsx
@@ -4,8 +4,10 @@ import axios from "axios";
 import {
   Box,
   Button,
+  Flex,
   FormControl,
   FormLabel,
+  Heading,
   Image,
   Input,
   Modal,
@@ -14,16 +16,24 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Spacer,
   Spinner,
   Textarea,
   useDisclosure,
   useToast,
 } from "@chakra-ui/react";
 import { LoginContext } from "../../component/LoginProvider.jsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as emptyHeart } from "@fortawesome/free-regular-svg-icons";
+import { faHeart as fullHeart } from "@fortawesome/free-solid-svg-icons";
 
 export function BoardView() {
   const { id } = useParams();
   const [board, setBoard] = useState(null);
+  const [like, setLike] = useState({
+    like: false,
+    count: 0,
+  });
   const account = useContext(LoginContext);
   const toast = useToast();
   const { isOpen, onClose, onOpen } = useDisclosure();
@@ -78,7 +88,21 @@ export function BoardView() {
 
   return (
     <Box>
-      <Box>{board.id}번 게시물</Box>
+      <Flex>
+        <Heading>{board.id}번 게시물</Heading>
+        <Spacer />
+        <Flex>
+          <Box
+            onClick={() => setLike({ ...like, like: !like.like })}
+            cursor={"pointer"}
+            fontSize={"3xl"}
+          >
+            {like.like && <FontAwesomeIcon icon={fullHeart} />}
+            {like.like || <FontAwesomeIcon icon={emptyHeart} />}
+          </Box>
+          <Box fontSize={"3xl"}>{like.count}</Box>
+        </Flex>
+      </Flex>
       <Box>
         <FormControl>
           <FormLabel>제목</FormLabel>
