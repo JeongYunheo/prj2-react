02fbf685e5b99f2a3d1bb39fcdd4daa54a048e56
jeongY <zoe1126@naver.com>
2024-05-30 15:29:23 +0900

    댓글 지우기

--- src/component/comment/CommentItem.jsx
+++ src/component/comment/CommentItem.jsx
@@ -1,6 +1,19 @@
-import { Box, Flex, Spacer } from "@chakra-ui/react";
+import { Box, Button, Flex, Spacer } from "@chakra-ui/react";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export function CommentItem({ comment }) {
+  function handleRemoveClick() {
+    axios
+      .delete(`/api/comments/remove`, {
+        data: { id: comment.id },
+      })
+      .then((res) => {})
+      .catch((err) => {})
+      .finally(() => {});
+  }
+
   return (
     <Box>
       <Box border={"1px solid black"} my={3}>
@@ -9,7 +22,18 @@ export function CommentItem({ comment }) {
           <Spacer />
           <Box>{comment.inserted}</Box>
         </Flex>
-        <Box>{comment.comment}</Box>
+        <Flex>
+          <Box>{comment.comment}</Box>
+          <Spacer />
+          <Box>
+            <Button
+              colorScheme={"red"}
+              onClick={() => handleRemoveClick(comment.id)}
+            >
+              <FontAwesomeIcon icon={faTrash} />
+            </Button>
+          </Box>
+        </Flex>
       </Box>
     </Box>
   );
